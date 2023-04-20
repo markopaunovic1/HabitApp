0283f1c04ec2ce917d09753e140fca226d61b6fd
markopaunovic1 <marpau970514@hotmail.com>
2023-04-20 15:23:18 +0200

    Initial Commit

---
+++ Habitapp/HabitappApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitappApp.swift
+//  Habitapp
+//
+//  Created by Marko Paunovic on 2023-04-20.
+//
+
+import SwiftUI
+
+@main
+struct HabitappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
